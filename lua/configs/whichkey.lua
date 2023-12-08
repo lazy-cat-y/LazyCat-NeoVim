d5333b5f87f1d2dcb5c0054d875a372c85a196a2
lazy-cat-y <yang_haoxin@outlook.com>
2023-12-08 21:54:51 +0800

    delete some terminal

--- lua/configs/whichkey.lua
+++ lua/configs/whichkey.lua
@@ -178,7 +178,7 @@ local mappings = {
 
     t = {
         name = "Terminal",
-        n = { "<cmd>lua _NODE_TOGGLE()<cr>", "Node" },
+        -- n = { "<cmd>lua _NODE_TOGGLE()<cr>", "Node" },
         u = { "<cmd>lua _NCDU_TOGGLE()<cr>", "NCDU" },
         t = { "<cmd>lua _HTOP_TOGGLE()<cr>", "Htop" },
         p = { "<cmd>lua _PYTHON_TOGGLE()<cr>", "Python" },
