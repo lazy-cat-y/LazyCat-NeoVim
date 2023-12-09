57dd511c83f870dedc2a1411593cdd5b4136f020
lazy-cat-y <yang_haoxin@outlook.com>
2023-12-09 16:53:32 +0800

    change cmp config

--- lua/configs/cmp-config.lua
+++ lua/configs/cmp-config.lua
@@ -119,6 +119,7 @@ cmp.setup {
         select = false,
     },
     window = {
+        completion = cmp.config.window.bordered(),
         documentation = cmp.config.window.bordered(),
     },
     experimental = {
