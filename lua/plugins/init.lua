911375be25b7b8fd1f3667d7b5262d8654b9c7d3
lazy-cat-y <lazyhaoxinyang@outlook.com>
2025-09-05 07:27:30 -0700

    nvim-surround

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -153,5 +153,15 @@ return {
             { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
             { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
         },
+    },
+    {
+        "kylechui/nvim-surround",
+        version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
     }
 }
