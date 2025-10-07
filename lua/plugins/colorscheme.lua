19a8cefafc93884d83e0c677e0526f98b0cb9b3a
lazy-cat-y <lazyhaoxinyang@outlook.com>
2025-10-07 08:32:01 -0700

    fix colorshceme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,10 +6,14 @@ return {
         priority = 1000,
         opts = {
             transparent_background = true,
+            float = {
+                transparent = true, -- enable transparent floating windows
+                solid = false, -- use solid styling for floating windows, see |winborder|
+            },
         },
         config = function(_, opts)
             require("catppuccin").setup(opts)
             vim.cmd.colorscheme("catppuccin-mocha")
-        end
+        end,
     },
 }
